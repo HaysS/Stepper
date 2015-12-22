2efd1f8aeac0c327e1bab1ae5b7b70f8243102ae
Hays <haysstanford@gmail.com>
2015-12-21 20:39:39 -0600

    Added test for form input for habit list to unit test and added some functionality

--- habit_lists/tests.py
+++ habit_lists/tests.py
@@ -1,5 +1,8 @@
+from django.http import HttpRequest
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.template.loader import render_to_string
+
 
 from habit_lists.views import home_page
 
@@ -8,3 +11,18 @@ class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+	
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
+
+	def test_home_page_can_save_a_POST_request(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['habit_text'] = 'A new habit'
+		
+		response = home_page(request)
+		
+		self.assertIn('A new habit', response.content.decode())
