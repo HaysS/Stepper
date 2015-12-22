9c80c56ca15d4e18801caf0c5e30a39b5dd512ee
Hays <haysstanford@gmail.com>
2015-12-21 20:11:52 -0600

    Added test for user input into habit lists in FT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -14,3 +14,15 @@ class NewVisitorTest(LiveServerTestCase):
 		
 		self.assertIn('Stepper', self.browser.title)
 		
+		new_habit_elem = self.browser.find_element_by_id('new_habit')
+		habit_list_table = self.browser.find_element_by_id('habit_list')
+		habit_list_rows = habit_list_table.find_elements_by_tag_name('tr')
+
+		#User enters "Brush teeth" as a new habit.
+		new_habit_elem.send_keys('Brush teeth')
+		new_habit_elem.send_keys(Keys.ENTER)
+		#Check to see if "Brush teeth" is in the list of habits.
+		self.browser.assertTrue(
+			any(row.text == 'Brush teeth' for row in rows)
+		)
+		
--- habit_lists/tests.py
+++ habit_lists/tests.py
@@ -6,4 +6,5 @@ from habit_lists.views import home_page
 class HomePageTest(TestCase):
 	
 	def test_root_url_resolves_to_home_page_view(self):
-		pass
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
